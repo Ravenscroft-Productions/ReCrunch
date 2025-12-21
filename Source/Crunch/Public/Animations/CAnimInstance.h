7d1c9bc70083c031e3c21a4c1a492b85062330e0
Ravenscroft-Productions <steven.print@btinternet.com>
2025-12-21 13:45:10 +0000

    Add the Speed Attribute to drive Ground Locomotion

--- Source/Crunch/Public/Animations/CAnimInstance.h
+++ Source/Crunch/Public/Animations/CAnimInstance.h
@@ -19,10 +19,21 @@ public:
 	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
 	virtual void NativeThreadSafeUpdateAnimation(float DeltaSeconds) override;
 	
+	UFUNCTION(BlueprintCallable, meta=(BlueprintThreadSafe))
+	FORCEINLINE float GetSpeed() const { return Speed; }
+	
+	UFUNCTION(BlueprintCallable, meta=(BlueprintThreadSafe))
+	FORCEINLINE bool IsMoving() const { return Speed != 0.0f; }
+	
+	UFUNCTION(BlueprintCallable, meta=(BlueprintThreadSafe))
+	FORCEINLINE bool IsNotMoving() const { return Speed == 0.0f; }
+	
 private:
 	UPROPERTY()
 	ACharacter* OwnerCharacter;
 	
 	UPROPERTY()
 	UCharacterMovementComponent* OwnerMovementComp;
+	
+	float Speed;
 };
