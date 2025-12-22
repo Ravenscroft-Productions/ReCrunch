29b5286626e8019d486be35a8f66972232a242e9
Ravenscroft-Productions <steven.print@btinternet.com>
2025-12-22 17:49:10 +0000

    Setup the Server Client Call Chain

--- Source/Crunch/Public/Character/CCharacter.h
+++ Source/Crunch/Public/Character/CCharacter.h
@@ -18,6 +18,8 @@ class CRUNCH_API ACCharacter : public ACharacter, public IAbilitySystemInterface
 public:
 	// Sets default values for this character's properties
 	ACCharacter();
+	void ServerSideInit();
+	void ClientSideInit();
 
 protected:
 	// Called when the game starts or when spawned
@@ -37,7 +39,7 @@ public:
 	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override;
 
 private:
-	UPROPERTY()
+	UPROPERTY(VisibleDefaultsOnly, Category = "Gameplay Ability")
 	UCAbilitySystemComponent* CAbilitySystemComponent;
 	
 	UPROPERTY()
--- Source/Crunch/Public/Player/CPlayerController.h
+++ Source/Crunch/Public/Player/CPlayerController.h
@@ -6,6 +6,7 @@
 #include "GameFramework/PlayerController.h"
 #include "CPlayerController.generated.h"
 
+class ACPlayerCharacter;
 /**
  * 
  */
@@ -13,4 +14,14 @@ UCLASS()
 class CRUNCH_API ACPlayerController : public APlayerController
 {
 	GENERATED_BODY()
+public:
+	// Only called on the Server
+	virtual void OnPossess(APawn* NewPawn) override;
+	
+	// Only called on the Client, also on the Listening Server
+	virtual void AcknowledgePossession(APawn* NewPawn) override;
+	
+private:
+	UPROPERTY()
+	ACPlayerCharacter* CPlayerCharacter;
 };
