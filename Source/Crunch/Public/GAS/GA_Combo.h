4f7cd637fc36b77517dc1776d5aa121f4cfac17f
Ravenscroft-Productions <steven.print@btinternet.com>
2025-12-24 12:57:31 +0000

    Add the Animation Montage Play for the Combo Ability

--- Source/Crunch/Public/GAS/GA_Combo.h
+++ Source/Crunch/Public/GAS/GA_Combo.h
@@ -15,4 +15,8 @@ class CRUNCH_API UGA_Combo : public UCGameplayAbility
 	GENERATED_BODY()
 public:
 	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
+
+private:
+	UPROPERTY(EditDefaultsOnly, Category = "Animation")
+	UAnimMontage* ComboMontage;
 };
