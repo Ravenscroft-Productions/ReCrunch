7dfeceb4109c5b9b0206c1de622fb0161446d9b4
Ravenscroft-Productions <steven.print@btinternet.com>
2025-12-27 15:20:20 +0000

    Create the Animation Notify to Send Sweep Location Data in a Gameplay Event

---
+++ Source/Crunch/Public/Animations/AN_SendTargetGroup.h
@@ -0,0 +1,26 @@
+// (c) 2025-6 Ravenscroft-Productions
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+#include "Animation/AnimNotifies/AnimNotify.h"
+#include "AN_SendTargetGroup.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CRUNCH_API UAN_SendTargetGroup : public UAnimNotify
+{
+	GENERATED_BODY()
+public:
+	virtual void Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, const FAnimNotifyEventReference& EventReference) override;
+	
+private:
+	UPROPERTY(EditAnywhere, Category = "Gameplay Ability")
+	FGameplayTag EventTag;
+	
+	UPROPERTY(EditAnywhere, Category = "Gameplay Ability")
+	TArray<FName> TargetSocketNames;
+};
