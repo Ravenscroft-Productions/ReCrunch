428f562d093beb5cad88e7dffb7a276ac1a15069
Ravenscroft-Productions <steven.print@btinternet.com>
2026-01-02 10:49:50 +0000

    Add the Behaviour Tree and Blackboard to the AI and Update the Target

--- Source/Crunch/Private/AI/CAIController.cpp
+++ Source/Crunch/Private/AI/CAIController.cpp
@@ -3,6 +3,7 @@
 
 #include "AI/CAIController.h"
 
+#include "BehaviorTree/BlackboardComponent.h"
 #include "Character/CCharacter.h"
 #include "Perception/AIPerceptionComponent.h"
 #include "Perception/AISenseConfig_Sight.h"
@@ -25,6 +26,7 @@ ACAIController::ACAIController()
 	SightConfig->PeripheralVisionAngleDegrees = 180.0f;
 	
 	AIPerceptionComponent->ConfigureSense(*SightConfig);
+	AIPerceptionComponent->OnTargetPerceptionUpdated.AddDynamic(this, &ACAIController::TargetPerceptionUpdated);
 }
 
 void ACAIController::OnPossess(APawn* NewPawn)
@@ -37,5 +39,51 @@ void ACAIController::OnPossess(APawn* NewPawn)
 	if (PawnTeamInterface)
 	{
 		PawnTeamInterface->SetGenericTeamId(GetGenericTeamId());
+	}
+}
+
+void ACAIController::BeginPlay()
+{
+	Super::BeginPlay();
+	RunBehaviorTree(BehaviourTree);
+}
+
+void ACAIController::TargetPerceptionUpdated(AActor* TargetActor, FAIStimulus Stimulus)
+{
+	if (Stimulus.WasSuccessfullySensed())
+	{
+		if (!GetCurrentTarget())
+		{
+			SetCurrentTarget(TargetActor);
+		}
+	}
+	else
+	{
+		if (GetCurrentTarget() == TargetActor) SetCurrentTarget(nullptr);
+	}
+}
+
+const UObject* ACAIController::GetCurrentTarget() const
+{
+	const UBlackboardComponent* BlackboardComponent = GetBlackboardComponent();
+	if (BlackboardComponent)
+	{
+		return GetBlackboardComponent()->GetValueAsObject(TargetBlackboardKeyName);
+	}
+	return nullptr;
+}
+
+void ACAIController::SetCurrentTarget(AActor* NewTarget)
+{
+	UBlackboardComponent* BlackboardComponent = GetBlackboardComponent();
+	if (!BlackboardComponent) return;
+	
+	if (NewTarget)
+	{
+		BlackboardComponent->SetValueAsObject(TargetBlackboardKeyName, NewTarget);
+	}
+	else
+	{
+		BlackboardComponent->ClearValue(TargetBlackboardKeyName);
 	}
 }
--- Source/Crunch/Public/AI/CAIController.h
+++ Source/Crunch/Public/AI/CAIController.h
@@ -16,11 +16,24 @@ class CRUNCH_API ACAIController : public AAIController
 public:
 	ACAIController();
 	virtual void OnPossess(APawn* NewPawn) override;
+	virtual void BeginPlay() override;
 	
 private:
+	UPROPERTY(EditDefaultsOnly, Category = "AI Behaviour")
+	UBehaviorTree* BehaviourTree;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "AI Behaviour")
+	FName TargetBlackboardKeyName = "Target";
+	
 	UPROPERTY(VisibleDefaultsOnly, Category = "Perception")
 	UAIPerceptionComponent* AIPerceptionComponent;
 	
 	UPROPERTY(VisibleDefaultsOnly, Category = "Perception")
 	UAISenseConfig_Sight* SightConfig;
+	
+	UFUNCTION()
+	void TargetPerceptionUpdated(AActor* TargetActor, FAIStimulus Stimulus);
+	
+	const UObject* GetCurrentTarget() const;
+	void SetCurrentTarget(AActor* NewTarget);
 };
