586d40c2e6dc4cfd95debed1cbca8c0157f6dd9c
Ravenscroft-Productions <steven.print@btinternet.com>
2026-01-31 10:08:33 +0000

    Add the Ground Blast Ability

---
+++ Source/Crunch/Public/GAS/GA_GroundBlast.h
@@ -0,0 +1,22 @@
+// (c) 2025-6 Ravenscroft-Productions
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CGameplayAbility.h"
+#include "GA_GroundBlast.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CRUNCH_API UGA_GroundBlast : public UCGameplayAbility
+{
+	GENERATED_BODY()
+public:
+	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
+	
+private:
+	UPROPERTY(EditDefaultsOnly, Category = "Animation")
+	UAnimMontage* GroundBlastMontage;
+};
