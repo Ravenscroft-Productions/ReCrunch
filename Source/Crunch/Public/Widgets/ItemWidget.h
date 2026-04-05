64e157180c9b07ae42aa5c9499bbdbc63760d215
Ravenscroft-Productions <steven.print@btinternet.com>
2026-04-05 13:48:22 +0100

    Create and Test the Item Widget Class

---
+++ Source/Crunch/Public/Widgets/ItemWidget.h
@@ -0,0 +1,29 @@
+// (c) 2025-6 Ravenscroft-Productions
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "ItemWidget.generated.h"
+
+class UImage;
+/**
+ * 
+ */
+UCLASS()
+class CRUNCH_API UItemWidget : public UUserWidget
+{
+	GENERATED_BODY()
+public:
+	virtual void NativeConstruct() override;
+	virtual void SetIcon(UTexture2D* IconTexture);
+	
+private:
+	UPROPERTY(meta = (BindWidget))
+	UImage* ItemIcon;
+	
+	virtual FReply NativeOnMouseButtonDown(const FGeometry& InGeometry, const FPointerEvent& InMouseEvent) override;
+	virtual FReply NativeOnMouseButtonUp(const FGeometry& InGeometry, const FPointerEvent& InMouseEvent) override;
+	virtual void RightButtonClicked();
+	virtual void LeftButtonClicked();
+};
