377ea17560469b50d0a41bdd1ef0d4af4da0757c
Ravenscroft-Productions <steven.print@btinternet.com>
2026-04-11 14:20:32 +0100

    Create and Store the New Item on Both the Client and the Server

--- Source/Crunch/Public/Inventory/InventoryComponent.h
+++ Source/Crunch/Public/Inventory/InventoryComponent.h
@@ -3,12 +3,15 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "InventoryItem.h"
 #include "Components/ActorComponent.h"
 #include "InventoryComponent.generated.h"
 
 
 class UPA_ShopItem;
 class UAbilitySystemComponent;
+
+DECLARE_MULTICAST_DELEGATE_OneParam(FOnItemAddedDelegate, UInventoryItem* /*NewItem*/);
 
 UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
 class CRUNCH_API UInventoryComponent : public UActorComponent
@@ -17,6 +20,7 @@ class CRUNCH_API UInventoryComponent : public UActorComponent
 
 public:
 	UInventoryComponent();	
+	FOnItemAddedDelegate OnItemAdded;
 	void TryPurchase(const UPA_ShopItem* ItemToPurchase);
 	float GetGold() const;
 	
@@ -27,9 +31,20 @@ private:
 	UPROPERTY()
 	UAbilitySystemComponent* OwnerAbilitySystemComponent;
 
+	UPROPERTY()
+	TMap<FInventoryItemHandle, UInventoryItem*> InventoryMap;
+		
 /***************************************************************/
 /*                          Server                             */
 /***************************************************************/
 	UFUNCTION(Server, Reliable, WithValidation)
 	void Server_Purchase(const UPA_ShopItem* ItemToPurchase);
+	void GrantItem(const UPA_ShopItem* NewItem);
+	
+/***************************************************************/
+/*                          Client                             */
+/***************************************************************/
+private:
+	UFUNCTION(Client, Reliable)
+	void Client_ItemAdded(FInventoryItemHandle AssignedHandle, const UPA_ShopItem* Item);
 };
