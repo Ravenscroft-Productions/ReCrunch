1191ac9ebc468534e79539194f9c3acf399d6509
Ravenscroft-Productions <steven.print@btinternet.com>
2026-05-08 15:14:13 +0100

    Create the Parameters and Utility Functions of the Item Tree Widget

--- Source/Crunch/Public/Widgets/ItemTreeWidget.h
+++ Source/Crunch/Public/Widgets/ItemTreeWidget.h
@@ -3,9 +3,12 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "TreeNodeInterface.h"
 #include "Blueprint/UserWidget.h"
 #include "ItemTreeWidget.generated.h"
 
+class UCanvasPanelSlot;
+class UCanvasPanel;
 /**
  * 
  */
@@ -13,4 +16,35 @@ UCLASS()
 class CRUNCH_API UItemTreeWidget : public UUserWidget
 {
 	GENERATED_BODY()
+private:
+	void ClearTree();
+	UUserWidget* CreateWidgetForNode(const ITreeNodeInterface* Node, UCanvasPanelSlot*& OutCanvasSlot);
+	void CreateConnection(const UUserWidget* From, UUserWidget* To);
+	
+	UPROPERTY(meta = (BindWidget))
+	UCanvasPanel* RootPanel;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Tree")
+	FVector2D NodeSize = FVector2D{ 60.0f };
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Tree")
+	FVector2D NodeGap = FVector2D{ 16.0f, 30.0f };
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Tree")
+	FLinearColor ConnectionColour = FLinearColor{ 0.8f, 0.8f, 0.8f, 1.0f };
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Tree")
+	float ConnectionThickness = 3.0f;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Tree")
+	FVector2D SourcePortLocalPos = FVector2D{ 0.5f, 0.9f };
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Tree")
+	FVector2D DestinationPortLocalPos = FVector2D{ 0.5f, 0.1f };
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Tree")
+	FVector2D SourcePortDirection = FVector2D{ 0.0f, 90.0f };
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Tree")
+	FVector2D DestinationPortDirection = FVector2D{ 0.0f, 90.0f };
 };
