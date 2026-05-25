7fd2613935e7b58b548c836df29e4491efda6f07
Ravenscroft-Productions <steven.print@btinternet.com>
2026-05-25 08:55:40 +0100

    Create the Render Actor Widget Class

---
+++ Source/Crunch/Public/Widgets/RenderActorWidget.h
@@ -0,0 +1,52 @@
+// (c) 2025-6 Ravenscroft-Productions
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "RenderActorWidget.generated.h"
+
+class ARenderActor;
+class USizeBox;
+class UImage;
+/**
+ * 
+ */
+UCLASS(Abstract)
+class CRUNCH_API URenderActorWidget : public UUserWidget
+{
+	GENERATED_BODY()
+protected:
+	virtual void NativePreConstruct() override;
+	virtual void NativeConstruct() override;
+	virtual void BeginDestroy() override;
+	
+private:
+	virtual void SpawnRenderActor() PURE_VIRTUAL(URenderActorWidget::SpawnRenderActor, );
+	virtual ARenderActor* GetRenderActor() const PURE_VIRTUAL(URenderActorWidget::GetRenderActor, return nullptr;);
+	void BeginRenderCapture();
+	void UpdateRender();
+	void StopRenderCapture();
+	void ConfigureRenderActor();
+	
+	UPROPERTY(meta = (BindWidget))
+	UImage* DisplayImage;
+	
+	UPROPERTY(meta = (BindWidget))
+	USizeBox* RenderSizeBox;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Render Actor")
+	FName DisplayImageRenderTargetParamName = "RenderTarget";
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Render Actor")
+	FVector2D RenderSize;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Render Actor")
+	int FrameRate = 24;
+	
+	UPROPERTY()
+	UTextureRenderTarget2D* RenderTarget;
+	
+	float RenderTickInterval;
+	FTimerHandle RenderTimerHandle;
+};
