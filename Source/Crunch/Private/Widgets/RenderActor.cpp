c8c3eae2ecce622a207c1278d5a244f804e2f5a5
Ravenscroft-Productions <steven.print@btinternet.com>
2026-05-25 08:20:15 +0100

    Create the Render Actor Class

---
+++ Source/Crunch/Private/Widgets/RenderActor.cpp
@@ -0,0 +1,39 @@
+// (c) 2025-6 Ravenscroft-Productions
+
+
+#include "Widgets/RenderActor.h"
+
+#include "Components/SceneCaptureComponent2D.h"
+
+
+ARenderActor::ARenderActor()
+{
+	PrimaryActorTick.bCanEverTick = true;
+	RootComp = CreateDefaultSubobject<USceneComponent>("Root Comp");
+	SetRootComponent(RootComp);
+	
+	CaptureComponent = CreateDefaultSubobject<USceneCaptureComponent2D>("Capture Component");
+	CaptureComponent->SetupAttachment(RootComp);
+	
+	CaptureComponent->bCaptureEveryFrame = false;
+	CaptureComponent->FOVAngle = 30.0f;
+}
+
+void ARenderActor::SetRenderTarget(UTextureRenderTarget2D* RenderTarget)
+{
+	CaptureComponent->TextureTarget = RenderTarget;
+}
+
+void ARenderActor::UpdateRender()
+{
+	if (CaptureComponent)
+	{
+		CaptureComponent->CaptureScene();
+	}
+}
+
+void ARenderActor::BeginPlay()
+{
+	Super::BeginPlay();
+	CaptureComponent->ShowOnlyActorComponents(this);
+}
