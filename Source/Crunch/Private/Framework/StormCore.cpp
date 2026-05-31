f015ee95d3d41ca2c69f66d84d68f2fa89e8037c
Ravenscroft-Productions <steven.print@btinternet.com>
2026-05-31 16:37:28 +0100

    Update the Team Count and Team Weight

--- Source/Crunch/Private/Framework/StormCore.cpp
+++ Source/Crunch/Private/Framework/StormCore.cpp
@@ -3,28 +3,81 @@
 
 #include "Framework/StormCore.h"
 
+#include "GenericTeamAgentInterface.h"
+#include "Components/SphereComponent.h"
 
-// Sets default values
+
 AStormCore::AStormCore()
 {
-	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+	InfluenceRange = CreateDefaultSubobject<USphereComponent>("Influence Range");
+	InfluenceRange->SetupAttachment(GetRootComponent());
+	InfluenceRange->OnComponentBeginOverlap.AddDynamic(this, &AStormCore::NewInfluencerInRange);
+	InfluenceRange->OnComponentEndOverlap.AddDynamic(this, &AStormCore::InfluencerLeftRange);
 }
 
-// Called when the game starts or when spawned
 void AStormCore::BeginPlay()
 {
-	Super::BeginPlay();
-	
+	Super::BeginPlay();	
 }
 
-// Called every frame
+void AStormCore::NewInfluencerInRange(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	IGenericTeamAgentInterface* OtherTeamInterface = Cast<IGenericTeamAgentInterface>(OtherActor);
+	if (OtherTeamInterface)
+	{
+		if (OtherTeamInterface->GetGenericTeamId().GetId() == 0)
+		{
+			TeamOneInfluencerCount++;
+		}
+		else if (OtherTeamInterface->GetGenericTeamId().GetId() == 1)
+		{
+			TeamTwoInfluencerCount++;
+		}
+		UpdateTeamWeight();
+	}
+}
+
+void AStormCore::InfluencerLeftRange(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	IGenericTeamAgentInterface* OtherTeamInterface = Cast<IGenericTeamAgentInterface>(OtherActor);
+	if (OtherTeamInterface)
+	{
+		if (OtherTeamInterface->GetGenericTeamId().GetId() == 0)
+		{
+			TeamOneInfluencerCount--;
+			if (TeamOneInfluencerCount < 0) TeamOneInfluencerCount = 0;
+		}
+		else if (OtherTeamInterface->GetGenericTeamId().GetId() == 1)
+		{
+			TeamTwoInfluencerCount--;
+			if (TeamTwoInfluencerCount < 0) TeamTwoInfluencerCount = 0;
+		}
+		UpdateTeamWeight();
+	}
+}
+
+void AStormCore::UpdateTeamWeight()
+{
+	if (TeamOneInfluencerCount == TeamTwoInfluencerCount)
+	{
+		TeamWeight = 0.0f;
+	}
+	else
+	{
+		float TeamOffset = TeamOneInfluencerCount - TeamTwoInfluencerCount;
+		float TeamTotal = TeamOneInfluencerCount + TeamTwoInfluencerCount;
+		TeamWeight = TeamOffset / TeamTotal;
+	}
+	
+	UE_LOG(LogTemp, Warning, TEXT("Team One Count: %d, Team Two Count: %d, Weight: %f"), TeamOneInfluencerCount, TeamTwoInfluencerCount, TeamWeight);
+}
+
 void AStormCore::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 }
 
-// Called to bind functionality to input
 void AStormCore::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
--- Source/Crunch/Public/Framework/StormCore.h
+++ Source/Crunch/Public/Framework/StormCore.h
@@ -6,23 +6,34 @@
 #include "GameFramework/Character.h"
 #include "StormCore.generated.h"
 
+class USphereComponent;
+
 UCLASS()
 class CRUNCH_API AStormCore : public ACharacter
 {
 	GENERATED_BODY()
 
 public:
-	// Sets default values for this character's properties
 	AStormCore();
+	virtual void Tick(float DeltaTime) override;
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
 protected:
-	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-public:
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+private:
+	UPROPERTY(VisibleDefaultsOnly, Category = "Detection")
+	USphereComponent* InfluenceRange;
+	
+	UFUNCTION()
+	void NewInfluencerInRange(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 
-	// Called to bind functionality to input
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	UFUNCTION()
+	void InfluencerLeftRange(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+	
+	void UpdateTeamWeight();
+	
+	int TeamOneInfluencerCount = 0;
+	int TeamTwoInfluencerCount = 0;
+	float TeamWeight = 0.0f;
 };
