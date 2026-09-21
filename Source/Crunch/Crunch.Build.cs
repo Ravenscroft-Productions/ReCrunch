25d2dd2e92a0b4466ff3e74e8e2cc7d808b9d5ef
Ravenscroft-Productions <steven.print@btinternet.com>
2026-09-21 21:32:46 +0100

    Create the Lazer Target Actor

--- Source/Crunch/Crunch.Build.cs
+++ Source/Crunch/Crunch.Build.cs
@@ -8,7 +8,7 @@ public class Crunch : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "GameplayAbilities", "AIModule" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "GameplayAbilities", "AIModule", "Niagara" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] { "GameplayTasks", "GameplayTags", "UMG" });
 		
---
+++ Source/Crunch/Public/GAS/TargetActor_Line.h
@@ -0,0 +1,30 @@
+﻿// (c) 2025-6 Ravenscroft-Productions
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/GameplayAbilityTargetActor.h"
+#include "TargetActor_Line.generated.h"
+
+class USphereComponent;
+class UNiagaraComponent;
+
+UCLASS()
+class CRUNCH_API ATargetActor_Line : public AGameplayAbilityTargetActor
+{
+	GENERATED_BODY()
+public:
+	ATargetActor_Line();
+	
+private:
+	UPROPERTY(VisibleDefaultsOnly, Category = "Component")
+	USceneComponent* RootComp;
+	
+	UPROPERTY(VisibleDefaultsOnly, Category = "Component")
+	UNiagaraComponent* LazerVFX;
+	
+	UPROPERTY(VisibleDefaultsOnly, Category = "Component")
+	USphereComponent* TargetEndDetectionSphere;
+	
+	const AActor* AvatarActor;
+};
